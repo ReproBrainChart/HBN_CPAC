f0a74a71439af401e5af1d2a1df9aed6ccffb55c
kahinimehta <kahinimehta@hotmail.com>
2024-05-30 14:24:18 -0400

    update

--- generate_CPAC_QC.py
+++ generate_CPAC_QC.py
@@ -20,8 +20,7 @@ for file in files:
         try:
                 df["medianFD"]=(pd.DataFrame.median(pd.read_table(median_file[0])))[0]
         except:
-                print(median_file[0])
-                os.system('datalad get '+median_file[0])
+                os.system('datalad get'+median_file[0])
                 df["medianFD"]=(pd.DataFrame.median(pd.read_table(median_file[0])))[0]                
         df_list.append(df)
 
@@ -35,10 +34,10 @@ df["motionExclude"][df["medianFD"]>=0.2]=1
 df["normCrossCorrExclude"][df["normCrossCorr"]<=0.8]=1
 df["fmriExclude"][df["normCrossCorrExclude"]==1]=1
 df["fmriExclude"][df["motionExclude"]==1]=1
+combined_df=df.rename(columns={"sub": "participant_id", "ses": "session_id"})
 
-combined_df=df
 
 print('The number of runs in this .csv is '+str(num))
 path = '/cbica/projects/RBC/QC_Concatenation/'+str(study_path)+'/cpac_RBCv0/study-'+study+'_desc-functional_qc.tsv'
 print ('The path to the csv is '+str(path))
-combined_df.to_csv(path,sep="\t")
+combined_df.to_csv(path,sep="\t",index=False)
